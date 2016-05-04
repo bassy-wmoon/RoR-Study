5fc05921ad4c9d97047a56e666b562b07fd4c5b2
Tomoharu Ishibashi <double.moon84@gmail.com>
2016-05-04 13:38:19 +0900

    レスポンスとしてXMLを返却できるように修正

--- app/controllers/api/helloworld_controller.rb
+++ app/controllers/api/helloworld_controller.rb
@@ -1,11 +1,11 @@
 class Api::HelloworldController < ApplicationController
-
-  def hello
-    
+  def helloJson
     response = {'key1' => 'hello', 'key2' => 'world'}
-    
     render :json => response
-    
   end
-
+  
+  def helloXml
+    response = {'key1' => 'hello', 'key2' => 'world'}
+    render :xml => response
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,8 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    get 'helloworld/hello'
+    get 'helloworld/helloJson'
+    get 'helloworld/helloXml'
   end
 
   get 'page/list'
